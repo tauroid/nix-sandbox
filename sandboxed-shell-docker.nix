83d2a954554c44d466c21ee5e21ce967216e6511
Adam Jacobs <adamjacobs173@googlemail.com>
2023-11-02 14:41:19 +0000

    Missed backslash

--- sandboxed-shell-docker.nix
+++ sandboxed-shell-docker.nix
@@ -40,7 +40,7 @@ in (import ./sandboxed-shell.nix) {
       -e HOST_GID=$UID \
       ${envVarArgs} \
       --add-host=host.docker.internal:host-gateway \
-      ${builtins.concatStringsSep " " extraDockerArgs}
+      ${builtins.concatStringsSep " " extraDockerArgs} \
       $IMAGE \
       ${pkgs.bashInteractive}/bin/bash \
         -c ${script}/bin/enterNormalUserShellScript
